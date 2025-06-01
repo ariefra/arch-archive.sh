971c8735f1045d4c6e414e300fa08e58b745f406
ariefra <arief.r.id@gmail.com>
2025-06-01 17:11:27 +0300

    refactor to python by vibe coding with gemini

--- arch-archive.sh
+++ arch-archive.sh
@@ -5,10 +5,11 @@ DIR=/etc/pacman.d
 
 usage(){
 echo -e "this script requires rw access to /etc/pacman.d/ and /etc/pacman.conf, and bc to calculates dates
-Usage:\tarch-archive.sh { -s | -f n | -u}
+Usage:\tarch-archive.sh [ -h | -f [n] | -u | -s ]
+\t-h\tShow this help
 \t-f n\tFreeze repo to previous n days, n default to 0
 \t-u\tUnfreeze repo and return to current mirrorlist
-\t-i\tShow current repo"
+\t-s\tUpdate now & Freeze again"
 }
 
 info(){
@@ -21,20 +22,21 @@ info(){
 }
 
 unfreeze(){
+  info
   echo "replacing occurance of archive to mirrorlist in /etc/pacman.conf"
   sed -i 's/archive/mirrorlist/' /etc/pacman.conf
-  pacman -Syyu
+  pacman -Syu --noconfirm
 }
 
 freeze(){
   echo "linking archive repository"
-  days=${1:-0}
-  date=$(date --date=@$(echo "$(date +%s)-86400*($days+1)" | bc) +%Y/%m/%d)
+  days=${1:-1}
+  date=$(date --date=@$(echo "$(date +%s)-61200*($days)" | bc) +%Y/%m/%d)
   rm /etc/pacman.d/archive -f
   echo -e "SigLevel = PackageRequired\nServer=https://archive.archlinux.org/repos/$date/\$repo/os/\$arch\n" > /etc/pacman.d/archive
   sed -i 's/mirrorlist/archive/' /etc/pacman.conf
+  pacman -Syu --noconfirm
   info
-  pacman -Syyu
 }
 
 if [[ "$EUID" -ne 0 || ! -r "$FILE" && ! -w "$FILE" || ! -w "$DIR" ]] then 
@@ -49,10 +51,12 @@ case "$1" in
   "-u")
     unfreeze
     ;;
-  "-i")
-    info
+  "-s")
+    unfreeze
+    freeze
     ;;
   *)
+    info
     usage
     ;;
 esac
